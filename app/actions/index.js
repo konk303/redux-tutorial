62866e98af6885e2bffebcbb36b14541af701d4e
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2016-12-05 20:26:03 +0900

    complete redux <-> react example, without async

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,6 +2,7 @@ export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
+let nextTodoId = 0;
 
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
@@ -10,11 +11,11 @@ export const VisibilityFilters = {
 };
 
 export function addTodo(text) {
-  return { type: ADD_TODO, text };
+  return { type: ADD_TODO, id: nextTodoId ++, text };
 }
 
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index };
+export function toggleTodo(id) {
+  return { type: TOGGLE_TODO, id };
 }
 
 export function setVisibilityFilter(filter) {
